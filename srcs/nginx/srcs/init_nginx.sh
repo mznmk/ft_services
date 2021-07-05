b0a456579952dfe8384b1c8ca6d0dc4d17017dc0
mznmk <mika_develop@mznmk.com>
2021-07-05 22:13:00 +0900

    [nginx] complete to create prototype version

--- srcs/nginx/srcs/init_nginx.sh
+++ srcs/nginx/srcs/init_nginx.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# modify telegraf setting
+sed -i -e s/'hostname = ""'/'hostname = "nginx-service"'/ /etc/telegraf.conf
+sed -i -e s/'database = ""'/'database = "nginx"'/ /etc/telegraf.conf
+
+# start telegraf
+telegraf --config /etc/telegraf.conf &
+
 # avoid Nginx to be daemon
 nginx -g 'daemon off;'
