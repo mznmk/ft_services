2f23eefeda88f44cbffbf6e33dbc1f73c9dae344
mznmk <mika_develop@mznmk.com>
2021-07-06 17:18:58 +0900

    [clean] complete to create prototype version

--- clean.sh
+++ clean.sh
@@ -6,18 +6,11 @@
 
 ESC_RESET='\033[0m'
 ESC_FNT_BOLD='\033[1m'
-ESC_FNT_FINE='\033[2m'
-ESC_FNT_ITALIC='\033[3m'
-ESC_FNT_ULINE='\033[4m'
-ESC_CLR_BLACK='\033[38;5;00m'
-ESC_CLR_RED	='\033[38;5;01m'
 ESC_CLR_GREEN='\033[38;5;02m'
 ESC_CLR_YELLOW='\033[38;5;03m'
-ESC_CLR_BLUE='\033[38;5;04m'
-ESC_CLR_MAGENTA='\033[38;5;05m'
 ESC_CLR_CYAN='\033[38;5;06m'
-ESC_CLR_WHITE='\033[38;5;07m'
-ESC_CLR_PINK='\033[38;5;213m'
+
+# ================================= Scripts ================================== #
 
 # ============================= Title ASCII ART ============================== #
 
@@ -28,26 +21,54 @@ echo '| (__| |  __/ (_| | | | |'
 echo ' \___|_|\___|\__,_|_| |_|'
 echo "${ESC_RESET}"
 
+# ============================ working directory ============================= #
+
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Working Directory] start ..${ESC_RESET}"
+
+rm -rf /data
+
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Working Directory] finish !!${ESC_RESET}"
+echo ""
+
+# =================================== logs =================================== #
+
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Install Logs] start !!${ESC_RESET}"
+
+rm -rf ./logs/*.log
+
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Install Logs] finish !!${ESC_RESET}"
+echo ""
+
 # ================================ kubernetes ================================ #
 
-echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Stop Kubernetes Setting] start !!${ESC_RESET}"
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Kubernetes Setting] start !!${ESC_RESET}"
 
-kubectl delete pods --all
-kubectl delete services --all
-kubectl delete deployments --all
-kubectl delete PersistentVolumeClaim --all
+kubectl delete pod --all
+kubectl delete service --all
+kubectl delete deployment --all
+# kubectl delete pv --all
+kubectl delete pvc --all
+kubectl delete secret --all
 
-echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Stop Kubernetes Setting] finish !!${ESC_RESET}"
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Kubernetes Setting] finish !!${ESC_RESET}"
+echo ""
 
 # ================================== docker ================================== #
 
-echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Stop Docker Setting] start !!${ESC_RESET}"
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Docker Image] start !!${ESC_RESET}"
 
+docker image rm ft_influxdb
+docker image rm ft_grafana
+docker image rm ft_ftps
+docker image rm ft_wordpress
+docker image rm ft_mysql
+docker image rm ft_phpmyadmin
 docker image rm ft_nginx
 docker image rm alpineplus
 docker image rm alpine
 
-echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Stop Docker Setting] finish !!${ESC_RESET}"
+echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Delete Docker Image] finish !!${ESC_RESET}"
+echo ""
 
 # ================================= minikube ================================= #
 
@@ -57,5 +78,6 @@ minikube stop
 minikube delete
 
 echo "${ESC_FNT_BOLD}${ESC_CLR_GREEN}[Stop Minikube Setting] finish !!${ESC_RESET}"
+echo ""
 
 # ============================================================================ #
